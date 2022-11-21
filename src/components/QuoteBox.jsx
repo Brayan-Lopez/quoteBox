50e183d13d9b01de104d3aa8f639a75a99896f19
Brayan Lopez <brayan.lopez.pizza@gmail.com>
2022-11-21 11:40:55 -0500

    styles responsive update

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -4,11 +4,14 @@ import QuoteButton from './QuoteButton'
 const QuoteBox = ({quote, getNewValues, backgroundObject, fontColorObject}) => {
   return (
     <article className='quoteBox'>
-      <h1 style={fontColorObject}> <span>``</span> {quote.quote}</h1>
-      <h3 style={fontColorObject}>- {quote.author} -</h3>
-          <QuoteButton 
+      <div>
+        <h1 className='quote1' style={fontColorObject}> <span>``</span> {quote.quote}</h1>
+        <h2 className='quote2' style={fontColorObject}>``{quote.quote}´´</h2>
+        <h3 style={fontColorObject}>- {quote.author}</h3>
+      </div>
+        <QuoteButton 
           getNewValues={getNewValues} 
-          backgroundObject={backgroundObject}/>
+          backgroundObject={backgroundObject}/> 
     </article>
   )
 }
